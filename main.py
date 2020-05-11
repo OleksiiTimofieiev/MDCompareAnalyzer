bbe778ab6aa683460678a214bc99f821bfbaf431
Oleksii.Timofieiev <contrbas@gmail.com>
2020-05-11 14:50:42 +0300

    working on the project structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import sys
 from collections import deque
 
 from configsReader import ConfigsReader
@@ -10,9 +11,15 @@ AcceptableMismatchList = deque(ConfigsReader.getAcceptableMismatchList())
 # print(AcceptableMismatchList)
 
 def main():
-    while 1:
-        continue
-
+    if len(sys.argv) == 1:
+        print("Usage details: please add filename.")
+        sys.exit
+    elif len(sys.argv) > 2:
+        print("Usage details: only on file can be selected.")
+        sys.exit
+    else:
+        while 1:
+            continue
 
 if __name__ == "__main__":
     main()
